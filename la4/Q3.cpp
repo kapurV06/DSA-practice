b1788668060ac86b5545a9f83b90090016083564
VarishKapur <kapur.varish75@gmail.com>
2025-11-17 18:17:39 +0530

    Create Q3.cpp

---
+++ la4/Q3.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+      void interleaveQueue(queue<int> &q)
+{
+  int n = q.size();
+  if ( n% 2 != 0)
+  {
+      cout << "Queue size must be even\n";
+          return;
+  }
+  int half =n / 2;
+  queue<int>firstHalf;
+  for(int i = 0; i < half; i++)
+  {
+        firstHalf.push(q.front());
+    q.pop();
+  }
+  while (!firstHalf.empty())
+  {
+    q.push(firstHalf.front());
+       firstHalf.pop();
+      q.push(q.front());
+       q.pop();
+  }
+}
+
+int main(){
+  queue<int>q;
+    int n,x;
+       cout<<"Enter number of elements: ";
+  cin>>n;
+     for(int i = 0; i < n; i++)
+  {
+    cin>> x;
+      q.push(x);
+  }
+     interleaveQueue(q);
+  while (!q.empty())
+  {
+         cout << q.front() << " ";
+      q.pop();
+  }
+    return 0;
+}
